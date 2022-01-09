4152c5b70216242a1e10aa05ce6de828e218a6c8
Maria Saavedra <msaav3@gmail.com>
2022-01-08 21:01:57 -0600

    fix margin

--- components/Header.component.tsx
+++ components/Header.component.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <div className="inline-block mt-10">
+    <div className="inline-block ml-4 mt-5 md:mt-5 lg:mt-10">
       <a href="#"><img width="150px" src="/images/logo-black.png"></img></a>
     </div>
   );
